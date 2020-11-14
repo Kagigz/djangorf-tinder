b1113a30e2bac6abdd7f9671262f3ac76c2eafb4
Katia Gil Guzman <kagilguz@microsoft.com>
2020-11-14 23:47:59 +0100

    added basic app user model

--- tinderlikeapi/models.py
+++ tinderlikeapi/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class AppUser(models.Model):
+
+    email = models.EmailField()
+    password = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=500)
+    picture = models.ImageField()
+    gender = models.CharField(max_length=30,
+                choices=[
+                    ('female','Female'),
+                    ('male','Male'),
+                    ('nonbinary','Non-binary'),
+                    ('other','Other')
+                    ])
+    localisation = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.email

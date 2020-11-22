931eda2c719a33b41f3188321f2a2d5e946309ae
Katia Gil Guzman <kagilguz@microsoft.com>
2020-11-22 21:56:52 +0100

    updated appuser model to resize and blur profile pictures

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import settings
+from PIL import Image, ImageFilter
 
 class AppUser(models.Model):
     """
@@ -12,12 +12,31 @@ class AppUser(models.Model):
     picture = models.ImageField(upload_to="userpictures/")
     gender = models.CharField(max_length=30,
                 choices=[
-                    ('female','Female'),
-                    ('male','Male'),
-                    ('nonbinary','Non-binary'),
-                    ('other','Other')
+                    ('female', 'Female'),
+                    ('male', 'Male'),
+                    ('nonbinary', 'Non-binary'),
+                    ('other', 'Other')
                     ])
     localisation = models.CharField(max_length=200)
 
     def __str__(self):
         return self.email
+
+    def save(self, *args, **kwargs):
+        super(AppUser, self).save(*args, **kwargs)
+        picture_path = self.picture.path
+        self.modify_picture(picture_path)
+
+    def modify_picture(self, path, thumbnail_size=(600, 600), blur_radius=5):
+        """
+        Resizes profile picture to thumbnail_size, blurs it and saves it
+
+        Parameters:
+        path (string): picture path
+        thumbnail_size (int, int): size of the new picture (600x600 by default)
+        blur_radius (int): blur radius for the gaussian blur filter (5 by default)
+        """
+        img = Image.open(path)
+        img.thumbnail(thumbnail_size)
+        blurred_img = img.filter(ImageFilter.GaussianBlur(blur_radius))
+        blurred_img.save(path)

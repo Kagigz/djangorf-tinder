8f36ac7d214aca208e1d8808c26031790936ea35
Katia Gil Guzman <kagilguz@microsoft.com>
2020-11-23 10:55:38 +0100

    updated page size to test with smaller dataset

--- api/views.py
+++ api/views.py
@@ -31,7 +31,7 @@ class AppUserViewSet(viewsets.ModelViewSet):
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 15
+    page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
